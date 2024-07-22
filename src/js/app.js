8a0ce6b98d8cc70ff5d153d698545fbe94755679
Ayoub.Nadir <ayoub.nadir@atrebo.com>
2024-07-22 14:11:03 +0200

    add back to top button

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,19 @@ document.querySelectorAll('.navegacion a').forEach(anchor => {
       this.classList.add('active');
     });
   });
+
+  window.addEventListener('scroll', function() {
+    const backToTopButton = document.querySelector('.back-to-top');
+    if (window.pageYOffset > 100) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
